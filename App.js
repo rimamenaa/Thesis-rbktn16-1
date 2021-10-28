b17c601481ef1d0059a0241c4d7670b0f05c9467
rimamenaa <rimamenaa98@gmail.com>
2021-10-28 14:40:17 +0100

    pulling

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Home from "./src/components/Homepage/Home";
-import Signup from "./src/components/Signup/Signup";
+import Signup from "./src/components/signup/Signup";
 export default function App() {
   return (
 <View>
