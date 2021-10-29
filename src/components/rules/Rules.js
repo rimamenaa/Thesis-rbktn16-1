2742a23dd6929a42b944744e7cf44711e376e705
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-10-29 22:43:39 +0100

    scroll fixed in the phone

--- src/components/rules/Rules.js
+++ src/components/rules/Rules.js
@@ -7,6 +7,7 @@ import {
   Text,
   TouchableOpacity,
   View,
+  ScrollView,
 } from "react-native";
 import tailwind from "tailwind-rn";
 import roadrule from "../../../assets/roadrule.png";
@@ -17,60 +18,62 @@ import roadrule4 from "../../../assets/roadrule4.png";
 function Rules() {
   return (
     <SafeAreaView style={tailwind("h-full flex-col")}>
-      <View style={tailwind(" items-center ")}>
-        <Image
-          source={roadrule}
-          style={{
-            width: 220,
-            height: 300,
-          }}
-        />
+      <ScrollView>
+        <View style={tailwind(" items-center ")}>
+          <Image
+            source={roadrule}
+            style={{
+              width: 220,
+              height: 300,
+            }}
+          />
 
-        <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
-          Don’t text and ride! Pull over if you have to send a message or talk
-          on the phone. It's also not a good idea to weave in and out of cars.
-          Being aware and predictable reduces the chance of a crash.
-        </Text>
+          <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
+            Don’t text and ride! Pull over if you have to send a message or talk
+            on the phone. It's also not a good idea to weave in and out of cars.
+            Being aware and predictable reduces the chance of a crash.
+          </Text>
 
-        <Image
-          source={roadrule2}
-          style={{
-            width: 220,
-            height: 300,
-          }}
-        />
-        <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
-          Bicyclists must yield the right of way to pedestrians at crosswalks
-          and intersections. Use your bell to alert pedestrians of your presence
-          when necessary.
-        </Text>
+          <Image
+            source={roadrule2}
+            style={{
+              width: 220,
+              height: 300,
+            }}
+          />
+          <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
+            Bicyclists must yield the right of way to pedestrians at crosswalks
+            and intersections. Use your bell to alert pedestrians of your
+            presence when necessary.
+          </Text>
 
-        <Image
-          source={roadrule3}
-          style={{
-            width: 280,
-            height: 300,
-          }}
-        />
-        <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
-          Obey all traffic signals and signs, such as stopping at red lights and
-          stop signs.
-        </Text>
+          <Image
+            source={roadrule3}
+            style={{
+              width: 280,
+              height: 300,
+            }}
+          />
+          <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
+            Obey all traffic signals and signs, such as stopping at red lights
+            and stop signs.
+          </Text>
 
-        <Image
-          source={roadrule4}
-          style={{
-            width: 280,
-            height: 300,
-          }}
-        />
-        <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
-          Bicyclists are required by law to ride in the same direction as
-          motorists, except in signed and marked contraflow lanes. Be alert and
-          look out for car doors opening – it's best to stay at least 3-4 feet
-          from the parked cars.
-        </Text>
-      </View>
+          <Image
+            source={roadrule4}
+            style={{
+              width: 280,
+              height: 300,
+            }}
+          />
+          <Text style={tailwind(" text-1xl  items-center font-semibold py-8 ")}>
+            Bicyclists are required by law to ride in the same direction as
+            motorists, except in signed and marked contraflow lanes. Be alert
+            and look out for car doors opening – it's best to stay at least 3-4
+            feet from the parked cars.
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
