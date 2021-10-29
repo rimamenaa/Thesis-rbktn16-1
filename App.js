ac7c52dda1574db30b71860a171c71ab73373cb1
ElyesBenKhoud <elyesbenkhoud377@gmail.com>
2021-10-29 19:15:14 +0100

    nav bar working and moving from compo to another well

--- App.js
+++ App.js
@@ -1,28 +1,18 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native-tailwind";
-import Home from "./src/components/Homepage/Home";
-import { NativeRouter, Switch, Route } from "react-router-native";
-import Login from "./src/components/login/Login";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Commercial from "./src/components/commercial/Commercial";
+import Test from "./src/components/suggestion roads/Test";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NativeRouter>
-      <View>
-        <Text>
-          {" "}
-          <Home />
-        </Text>
-      </View>
-    </NativeRouter>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Ye Heni w5aya rani faddit" component={Test} />
+        <Stack.Screen name="Commercial" component={Commercial} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#2F3945 ",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
