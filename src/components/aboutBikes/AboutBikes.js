41f7981a699e7208dca6bb552c7e068ee056b684
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-02 23:46:54 +0100

    bikes as cards

--- src/components/aboutBikes/AboutBikes.js
+++ src/components/aboutBikes/AboutBikes.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { View, Text, Image, Card } from "react-native";
+import {
+  Box,
+  AspectRatio,
+  Image,
+  Center,
+  Stack,
+  Text,
+  View,
+  Card,
+  ScrollView,
+} from "native-base";
 
 function AboutBikes() {
   const [data, setData] = useState([]);
@@ -13,21 +23,35 @@ function AboutBikes() {
   }, []);
 
   return (
-    <View>
+    <Box>
       {data.map((bike, key) => {
         console.log(bike);
         return (
-          <View key={key}>
-            <Image
-              style={{ width: 400, height: 400 }}
-              source={{ uri: bike.photo }}
-            />
-            <Text>{bike.description} </Text>
-            <Text>{bike.category} </Text>
-          </View>
+          <ScrollView>
+            <Card>
+              <Text fontSize="2xl" marginBottom="3">
+                {bike.category}
+              </Text>
+              <AspectRatio ratio={16 / 9}>
+                <Image
+                  marginTop="5"
+                  rounded="lg"
+                  height="100%"
+                  width="100%"
+                  source={{ uri: bike.photo }}
+                  alt={"Loading..."}
+                />
+              </AspectRatio>
+              <Stack p="4" space={4}>
+                <Text fontWeight="500" fontWeight="400" marginTop="8">
+                  {bike.description}
+                </Text>
+              </Stack>
+            </Card>
+          </ScrollView>
         );
       })}
-    </View>
+    </Box>
   );
 }
 
