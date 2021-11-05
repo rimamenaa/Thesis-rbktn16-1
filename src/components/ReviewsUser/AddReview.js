ce82ce2c5818ff9dc210da1fd8c6f64e7678835f
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-11-05 09:56:06 +0100

    input captured

--- src/components/ReviewsUser/AddReview.js
+++ src/components/ReviewsUser/AddReview.js
@@ -17,46 +17,49 @@ import {
   Card,
   TextArea,
   AspectRatio,
+  TextInput,
+  KeyboardAvoidingView,
   View,
 } from "native-base";
 import { AntDesign } from "@expo/vector-icons";
 import { Rating, AirbnbRating } from "react-native-ratings";
-import KeyboardAvoidingWrapper from "./KeyboardAvoidingWrapper";
 import tw from "tailwind-react-native-classnames";
 
 const AddReview = () => {
   return (
-    <Box>
-      <AirbnbRating
-        count={5}
-        reviews={[1, 2, 3, 4, 5]}
-        defaultRating={5}
-        size={20}
-      />
-      <KeyboardAvoidingWrapper>
-        <TextArea
-          fontSize="md"
-          fontWeight="semibold"
-          _dark={{
-            color: "coolGray.50",
-          }}
-          _light={{
-            color: "coolGray.800",
-          }}
-          h={{ base: "20" }}
-          placeholder="Your review goes here"
-          w={{
-            base: "100%",
-            md: "25%",
-          }}
-          onChangeText={(text) => setInput(text)}
-          onSubmitEditing={() => {
-            alert(`Your review is:${input}`);
-          }}
-        />
-        <Button> Add Review</Button>
-      </KeyboardAvoidingWrapper>
-    </Box>
+    <KeyboardAvoidingView behavior="height">
+      <ScrollView>
+        <Box>
+          <AirbnbRating
+            count={5}
+            reviews={[1, 2, 3, 4, 5]}
+            defaultRating={5}
+            size={20}
+          />
+          <TextInput
+            fontSize="md"
+            fontWeight="semibold"
+            _dark={{
+              color: "coolGray.50",
+            }}
+            _light={{
+              color: "coolGray.800",
+            }}
+            h={{ base: "20" }}
+            placeholder="Your review goes here"
+            w={{
+              base: "100%",
+              md: "25%",
+            }}
+            onChangeText={(text) => setInput(text)}
+            onSubmitEditing={() => {
+              alert(`Your review is:${input}`);
+            }}
+          />
+          <Button> Add Review</Button>
+        </Box>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
